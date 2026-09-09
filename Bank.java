1e2cc88d4e06e0b010d6987879da94429eb3bf28
chaneee93 <chaneee93@gmail.com>
2026-09-09 10:02:51 +0900

    미션2-3 배너 한글

--- Bank.java
+++ Bank.java
@@ -1,5 +1,6 @@
 public class Bank {
     public static void main(String[] args) {
+        System.out.println("========== OO은행 ==========");
         Account a = new Account();
         a.owner = "홍길동";
         a.balance = 50000;
