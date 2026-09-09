1cca15d3e053f7727b9fa25541a0b6b9d4c1ec53
chaneee93 <chaneee93@gmail.com>
2026-09-09 13:17:35 +0900

    미션2-5 생성자와 오버로딩

--- Account.java
+++ Account.java
@@ -1,4 +1,13 @@
 public class Account {
     String owner;
     int balance;
+
+    public Account(String owner, int balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public Account(String owner) {
+        this(owner, 0);          // 위 생성자를 부른다 (잔액 0)
+    }
 }
--- Bank.java
+++ Bank.java
@@ -1,15 +1,11 @@
 public class Bank {
     public static void main(String[] args) {
-        System.out.println("========== OO은행 ==========");
-        Account a = new Account();
-        a.owner = "홍길동";
-        a.balance = 50000;
-
-        Account b = new Account();
-        b.owner = "김철수";
-        b.balance = 30000;
+        Account a = new Account("홍길동", 50000);
+        Account b = new Account("김철수", 30000);
+        Account c = new Account("이영희");      // 초기 입금 없음
 
         System.out.println(a.owner + " " + a.balance);
         System.out.println(b.owner + " " + b.balance);
+        System.out.println(c.owner + " " + c.balance);
     }
 }
